a1c727dd3ed7ed7fc9a38c1e326b3b49e3ddccea
Goraniya Karan <karangoraniya@gmail.com>
2023-08-28 00:10:49 +0530

    chore: network config added & etherscan API 🚀

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,29 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+require('dotenv').config();
+
+const { PRIVATE_KEY, ETHERSCAN_API_KEY, ARBITRUM_RPC, SEPOLIA_RPC } =
+  process.env;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
+  networks: {
+    mumbai: {
+      url: `https://rpc.ankr.com/polygon_mumbai`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${SEPOLIA_RPC}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    arbitrum_goerli: {
+      url: `https://arb-goerli.g.alchemy.com/v2/${ARBITRUM_RPC}`,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
